a488be6e4b663b580bb29b91a855376522c9b06c
keem-hyun <hw813@naver.com>
2023-04-08 20:06:25 +0900

    scrollview 간단히

---
+++ studyScrollView/studyScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  studyScrollView
+//
+//  Created by 김강현 on 2023/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var image1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
